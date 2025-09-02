26a89fd4bfacaa97dde67ebaf125c69a91d905b8
xiaoxipi <74850693+xiaoxipi-wxp@users.noreply.github.com>
2025-09-02 15:38:53 +0800

    first commit

---
+++ oj-modules/oj-system/src/main/java/edu/hdu/system/OjSystemApplication.java
@@ -0,0 +1,11 @@
+package edu.hdu.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjSystemApplication.class, args);
+    }
+}
